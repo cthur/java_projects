b8b15e0b9299a0441cbbaa46d00d7ca5490b9bfd
Corey <cthur.dev@gmail.com>
2015-07-14 13:54:09 -0700

    Created MyTestCase.java with methods
    Created the file MyTestCase.java
    Created a few methods within the file

---
+++ 00_JUnit/src/test/MyTestCase.java
@@ -0,0 +1,39 @@
+package test;
+
+import java.util.Vector;
+
+import junit.framework.TestCase;
+
+public class MyTestCase extends TestCase {
+
+	private Vector container;
+
+	@Override
+	protected void setUp() throws Exception {
+		container = new Vector();
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		// TODO Auto-generated method stub
+		super.tearDown();
+	}
+
+	public MyTestCase() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Vector getContainer() {
+		return container;
+	}
+
+	public void setContainer(Vector container) {
+		this.container = container;
+	}
+
+	public MyTestCase(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+}
